a3be400c5c5cbc87d643e2f09b0238f112cf8177
yinizhizhu <lijiahe19941013@sina.cn>
2017-11-02 14:51:19 +0800

    update

---
+++ MachineLearning/svm.py
@@ -0,0 +1,95 @@
+from sklearn import svm
+import numpy as np
+import random
+from time import clock
+import matplotlib.pyplot as plt
+
+start = clock()
+
+class perceptron():
+    def __init__(self, w1, w2, bias, yet):
+        '''
+        w1, w2 - the weights
+        bias - the bias
+        yet - the ratio of learning
+        '''
+        self.clf = svm.LinearSVC()
+        
+        self.x = [] #store the properties of the object
+        self.y = [] #store the kind of class
+        
+        self.generateData()
+        self.readData()
+        
+        self.doIt()
+        
+    def generateData(self):
+        '''
+        generate the source data:(convinient to get access to the data)
+            the separating hyperplane is 'y=x'
+            output the data into text file
+        '''
+        out = open('data.txt', 'w')
+        for i in xrange(100):
+            print >> out, '{0} {1} {2}'.format(i+random.random(), 
+                        i+random.uniform(1, 9)+1, 1)
+        
+        for i in xrange(100):
+            print >> out, '{0} {1} {2}'.format(i+random.random(), 
+                        i-random.uniform(1, 9)-1, -1)
+        out.close()
+        
+    def readData(self):
+        '''
+        readData: 
+            get the data in the source data
+        '''
+        read = open('data.txt', 'r')
+        for line in read.readlines():
+            one = line.strip('\n').split(' ')
+            self.x.append([float(one[0]), float(one[1])])
+            self.y.append(int(one[2]))
+        read.close()
+        
+        #change the type of self.x into np.array which is better for operation
+        #it's same with self.y
+        self.x = np.array(self.x)
+        self.y= np.array(self.y)
+    
+    def drawLine(self, y1, y2, mode):
+        gap = (y2-y1)/100.0
+        t = []
+        for i in xrange(101):
+            t.append(y1)
+            y1 += gap
+        plt.plot(t, mode)
+    
+    def drawData(self):
+#        self.drawLine(0, 101, 'r') #draw the x1+x2=0
+        for i in xrange(200):
+            if self.y[i] == 1:
+                plt.plot(self.x[i][0], self.x[i][1], 'ro')
+            else:
+                plt.plot(self.x[i][0], self.x[i][1], 'g^')
+        h = -self.clf.raw_coef_[0][2]/self.clf.raw_coef_[0][1]
+        t = -self.clf.raw_coef_[0][0]/self.clf.raw_coef_[0][1]*101+h
+        self.drawLine(h, t, 'b')
+        plt.xlabel('X')
+        plt.ylabel('Y')
+        plt.show()
+        
+    def doIt(self):
+        self.clf.fit(self.x, self.y)
+        print self.clf.predict([[-0.8, -1]])
+        print self.clf.class_weight
+        print self.clf.class_weight_
+        print self.clf.decision_function
+        print self.clf.loss
+        print self.clf.C
+        print self.clf.raw_coef_ #store the weight and bias
+
+tmp = perceptron(random.random(), random.random(), random.random(), 0.01)
+tmp.drawData()
+
+end = clock()
+print "The total time is: ", end-start
