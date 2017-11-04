5501525d10634317867e0f66c20f740f652cf100
yinizhizhu <lijiahe19941013@sina.cn>
2017-11-04 19:18:22 +0800

    update

--- Algorithms/Assignment4/B.cpp
+++ Algorithms/Assignment4/B.cpp
@@ -9,6 +9,88 @@ using namespace std;
 #define MAX_LEE 2147483647
 #define N 1001
 
+double dis;
+
+struct node {
+	double x, y;
+	node(int a, int b) : x(a), y(b) {}
+	double gap(node& t) {
+		double ans = 0, deltax, deltay;
+		deltax = x - t.x;
+		deltay = y - t.y;
+		return sqrt(deltax*deltax + deltay*deltay);
+	}
+};
+
+bool cmp(node& a, node& b) {
+	return a.x < b.x;
+}
+
+void step(vector<node>& a, vector<node>& b, int h, int t, int mid) {
+	double d;
+	int h_n, t_n, i, j;
+	for (h_n = h; h_n < mid; h_n++)
+		if (b[mid].x - a[h_n].x < dis)
+			break;
+	for (t_n = t; t_n > mid; t_n--)
+		if (b[t_n - 1].x - a[mid].x < dis)
+			break;
+	for (i = h_n; i < mid; i++)
+		for (j = mid; j < t_n; j++) {
+			d = a[i].gap(b[j]);
+			if (d < dis) dis = d;
+		}
+}
+
+void closest_pair(vector<node>& stations, vector<node>& agents, int h, int t) {
+	int n = t - h, mid;
+	if (n == 1) {
+		double d = stations[h].gap(agents[h]);
+		if (d < dis) dis = d;
+		return;
+	}
+	mid = h + n / 2;
+	closest_pair(stations, agents, h, mid);
+	closest_pair(stations, agents, mid, t);
+	step(stations, agents, h, t, mid);
+	step(agents, stations, h, t, mid);
+}
+
+int main() {
+	int i, t, n, x, y;
+	scanf("%d", &t);
+	for (; t; t--) {
+		scanf("%d", &n);
+		vector<node> stations;
+		for (i = 0; i < n; i++) {
+			scanf("%d %d", &x, &y);
+			stations.push_back(node(x, y));
+		}
+		vector<node> agents;
+		for (i = 0; i < n; i++) {
+			scanf("%d %d", &x, &y);
+			agents.push_back(node(x, y));
+		}
+		sort(stations.begin(), stations.end(), cmp);
+		sort(agents.begin(), agents.end(), cmp);
+		dis = double(MAX_LEE);
+		closest_pair(stations, agents, 0, n);
+		printf("%.3lf\n", dis);
+	}
+	return 0;
+}
+
+#include <iostream>
+#include <stdio.h>
+#include <math.h>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+#define MAX_LEE 2147483647
+#define N 1001
+
 struct node {
 	double x, y;
 	node(int a, int b) : x(a), y(b) {}
--- Algorithms/Assignment4/C.cpp
+++ Algorithms/Assignment4/C.cpp
@@ -9,6 +9,52 @@ using namespace std;
 #define MAX_LEE 2147483647
 #define N 1001
 
+int store[N], index[N];
+
+int maxN(int a, int b) { return (a > b ? a : b); }
+
+void longest(int h, int t) {
+	int i, j, n = t - h, mid;
+	if (n == 1) {
+		index[h] = 1;
+		return;
+	}
+	mid = h + n / 2;
+	longest(h, mid);
+	longest(mid, t);
+	for (j = mid; j < t; j++) {
+		n = index[j];
+		for (i = h; i < j; i++)
+			if (store[j] > store[i])
+				n = maxN(n, index[i] + 1);
+		index[j] = n;
+	}
+}
+
+int main() {
+	int i, n, ans = 1;
+	scanf("%d", &n);
+	for (i = 0; i < n; i++)
+		scanf("%d", store + i);
+	longest(0, n);
+	for (i = 0; i < n; i++)
+		if (index[i] > ans)
+			ans = index[i];
+	printf("%d\n", ans);
+	return 0;
+}
+
+#include <iostream>
+#include <stdio.h>
+#include <math.h>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+#define MAX_LEE 2147483647
+#define N 1001
+
 int longest(int store[], int index[], int n) {
 	int i, j, maxN, maxI;
 	for (i = 1; i < n; i++) {
