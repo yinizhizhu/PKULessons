306e4dd40ffd7f2711185d6020c91a5812a54fe3
yinizhizhu <lijiahe19941013@sina.cn>
2017-11-11 13:53:35 +0800

    update

--- Experimental_Statistics/HSV/colorSpace.m
+++ Experimental_Statistics/HSV/colorSpace.m
@@ -1,33 +1,100 @@
 clear;
 clc;
 
-fileNames = fopen('src\zzz.txt', 'r');
+str=pwd;
+index_dir=findstr(str,'\');
+str_temp=str(1:index_dir(end)-1);
+
+fileNames = fopen([str_temp '\src\zzz.txt'], 'r');
 
 colorhist = zeros(1,256);
 
-h = zeros(1, 256);
-s = zeros(1, 256);
-v = zeros(1, 256);
+tmp = [];
+
+t_h = [];
+s_h = [];
+v_h = [];
 
 i = 1;
 while feof(fileNames) == 0
     fprintf('The %dth iteration...\n', i);
-    fileName = ['src\' fgetl(fileNames)];
+    fileName = [str_temp '\src\' fgetl(fileNames)];
     f = imread(fileName);
-    [h, s, v] = hsv(h, s, v, f);
-    colorhist = colorhist + gethsv(f);
+    tmp(i) = size(f, 1)*size(f,2);
+%     imshow(f);
+    [h_s, s_s, v_s] = hsv(f);
+    t_h(i, :) = h_s;
+    s_h(i, :) = s_s;
+    v_h(i, :) = v_s;
+%     colorhist = colorhist + gethsv(f);
     i = i + 1;
 end
 fclose(fileNames);
 
-figure('numbertitle','off','name','Color');
-bar(colorhist);
+% figure('numbertitle','off','name','Color');
+% bar(colorhist);
+% 
+% % show the bar
+% for i=1:145
+%     figure('numbertitle','off','name', sprintf('Hue %d', i));
+%     bar(t_h(i, :));
+% end
+% 
+% for i=1:145
+%     figure('numbertitle','off','name', sprintf('Saturation %d', i));
+%     bar(s_h(i, :));
+% end
+% 
+% for i=1:145
+%     figure('numbertitle','off','name', sprintf('Value %d', i));
+%     bar(v_h(i, :));
+% end
 
-figure('numbertitle','off','name','Hue');
-bar(h);
+% output the bar into the text file
+hf = fopen('data\h.txt', 'w');
+fprintf('Checking the hue...\n');
+for i=1:145
+    check =  sum(t_h(i, :))-tmp(i);
+    if check
+        fprintf('Oh on! %d %d\n',i, check);
+    end
+    
+    fprintf(hf, '%d', t_h(i, 1)/tmp(i));
+    for j=2:360
+        fprintf(hf, ' %d', t_h(i, j)/tmp(i));
+    end
+    fprintf(hf, ' \n');
+end
+fclose(hf);
 
-figure('numbertitle','off','name','Saturation');
-bar(s);
+sf = fopen('data\s.txt', 'w');
+fprintf('Checking the saturation...\n');
+for i=1:145
+    check =  sum(s_h(i, :))-tmp(i);
+    if check
+        fprintf('Oh on! %d %d\n',i, check);
+    end
+    
+    fprintf(sf, '%f', s_h(i, 1)/tmp(i));
+    for j=2:256
+        fprintf(sf, ' %f', s_h(i, j)/tmp(i));
+    end
+    fprintf(sf, ' \n');
+end
+fclose(sf);
 
-figure('numbertitle','off','name','Value');
-bar(v);
+vf = fopen('data\v.txt', 'w');
+fprintf('Checking the value...\n');
+for i=1:145
+    check =  sum(v_h(i, :))-tmp(i);
+    if check
+        fprintf('Oh on! %d %d\n',i, check);
+    end
+    
+    fprintf(vf, '%d', v_h(i, 1)/tmp(i));
+    for j=2:256
+        fprintf(vf, ' %d', v_h(i, j)/tmp(i));
+    end
+    fprintf(vf, ' \n');
+end
+fclose(vf);
--- Experimental_Statistics/HSV/hsv.m
+++ Experimental_Statistics/HSV/hsv.m
@@ -1,27 +1,36 @@
-function [h, s, v] = hsv( h, s, v, f )
+function [h_s, s_s, v_s] = hsv( f )
 %HSV 此处显示有关此函数的摘要
 %   此处显示详细说明
 
-hsv = uint8(255*rgb2hsv(f)); 
-[row, col, dep] = size(hsv);
+h_s = zeros(1, 360);
+s_s = zeros(1, 256);
+v_s = zeros(1, 256);
+
+hsv = rgb2hsv(f);
+h = uint16(hsv(:,:,1)*359);
+s = uint8(hsv(:,:,2)*255);
+v = uint8(hsv(:,:,3)*255);
+
+[row, col] = size(h);
 for i=1:row
     for j=1:col
-        index = hsv(i, j, 1)+1;
-        h(index) = h(index)+1;
+        index = h(i, j)+1;
+%         fprintf('%d\n', index);
+        h_s(index) = h_s(index)+1;
     end
 end
 
 for i=1:row
     for j=1:col
-        index = hsv(i, j, 2)+1;
-        s(index) = s(index)+1;
+        index = s(i, j)+1;
+        s_s(index) = s_s(index)+1;
     end
 end
 
 for i=1:row
     for j=1:col
-        index = hsv(i, j, 3)+1;
-        v(index) = v(index)+1;
+        index = v(i, j)+1;
+        v_s(index) = v_s(index)+1;
     end
 end
 
