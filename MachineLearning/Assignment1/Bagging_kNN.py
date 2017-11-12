eea4837c5d6ccc64dec77c8e5c72bfae3cbfcf41
yinizhizhu <lijiahe19941013@sina.cn>
2017-11-12 15:22:34 +0800

    update

--- MachineLearning/Assignment1/Bagging_kNN.py
+++ MachineLearning/Assignment1/Bagging_kNN.py
@@ -1,10 +1,11 @@
+import random
 from math import *
 from time import clock
 from sklearn import neighbors
 
 start = clock()
 
-class AdaBoost():
+class Bagging():
     def __init__(self, n):
         '''
         The source data set
@@ -41,15 +42,13 @@ class AdaBoost():
         
         self.loadData()
         
-        self.AdaSample()
-        
-        self.weight = [1.0/self.test_n for i in xrange(self.test_n)]
-        
-        
         self.clf_n = n
-        self.clf = [neighbors.KNeighborsClassifier(n_neighbors = 15+i)
+        self.clf = [neighbors.KNeighborsClassifier(n_neighbors = 15)
                                                    for i in xrange(self.clf_n)]
 
+        self.BootstrapSample()
+        
+        
     def getMap(self):
         self.map = dict()
         self.verse_map = dict()
@@ -62,7 +61,6 @@ class AdaBoost():
                 self.verse_map[i] = one
                 i = i+1
         read.close()
-        self.train_test = [0 for j in xrange(len(self.map))]
 
     def insertData(self, words):
         kind = words[self.attr_len]
@@ -77,29 +75,32 @@ class AdaBoost():
         for i in xrange(len(self.data[kind])):
             print self.data[kind][i]
 
-    def AdaSample(self):
-        self.x = []
-        self.y = []
-        self.train_n = 0
+    def BootstrapSample(self):
+        self.x = [[] for i in xrange(self.clf_n)]
+        self.y = [[] for i in xrange(self.clf_n)]
+        self.train_n = [0 for i in xrange(self.clf_n)]
 
-        self.x_test = []
-        self.y_test = []
-        self.test_n = 0
+        self.x_test = [[] for i in xrange(self.clf_n)]
+        self.y_test = [[] for i in xrange(self.clf_n)]
+        self.test_n = [0 for i in xrange(self.clf_n)]
 
-        for i in xrange(len(self.map)):
-            n = len(self.data[i])
-            n_train = int(n*0.8)
-            self.train_test[i] = n_train
-            
-            for j in xrange(n_train):
-                self.x.append(self.data[i][j])
-                self.y.append(i)
-            self.train_n += n_train
-            
-            for j in xrange(n_train, n):
-                self.x_test.append(self.data[i][j])
-                self.y_test.append(i)
-            self.test_n += n - n_train
+        for k in xrange(self.clf_n):
+            for i in xrange(len(self.map)):
+                n = len(self.data[i])
+                index = [0 for j in xrange(n)]
+                while sum(index) < n/2:
+                    index[random.randint(0,n-1)] = 1
+                
+                for j in xrange(n):
+                    if index[j]:
+                        self.x[k].append(self.data[i][j])
+                        self.y[k].append(i)
+                    else:
+                        self.x_test[k].append(self.data[i][j])
+                        self.y_test[k].append(i)
+                n_train = sum(index)
+                self.train_n[k] += n_train
+                self.test_n[k] += n - n_train
 
     def loadData(self):
         data = open(self.input_file, 'r')
@@ -109,66 +110,17 @@ class AdaBoost():
             self.insertData(words)
         data.close()
 
-    def I(self, value):
-        if value != 0:
-            return 1
-        return 0
-
-    def judge(self, value):
-        return self.verse_map[value]
-
-    def G(self, m, i):
-        return self.clf[m].predict(self.x_test[i])[0]
-
-    def Em(self, m):
-        em = 0.0
-        G_m = []
-        for i in xrange(self.test_n):
-            tmp = self.G(m, i)
-            G_m.append(tmp)
-            if tmp != self.y_test[i]:
-                em = em + self.weight[i]
-        return [em, G_m]
-
-    def Aerfa(self, m):
-#        print m, 
-        [em, G_m] = self.Em(m)
-#        print em
-        return [0.5*log((1-em)/em), G_m]
-
-    def step(self, m):
-        print self.weight[0]
-        [aerfa, G_m] = self.Aerfa(m)
-        z = 0
-        for i in xrange(self.test_n):
-            if self.y_test[i] == G_m[i]:
-                tmp = self.weight[i]*exp(-aerfa)
-            else:
-                tmp = self.weight[i]*exp(aerfa)
-            self.weight[i] = tmp
-            z = z+tmp
-        for i in xrange(self.test_n):
-            self.weight[i] /= z
-#        print aerfa
-        self.a_m.append(aerfa)
-
     def main(self):
-        self.a_m = []
         for m in xrange(self.clf_n):
             s = clock()
-            self.clf[m].fit(self.x, self.y)
-            self.step(m)
+            self.clf[m].fit(self.x[m], self.y[m])
             e = clock()
-            print self.a_m
             print "The step time is: ", e-s
 
     def finalG(self, i):
         ans = [0 for j in xrange(len(self.map))]
         for j in xrange(self.clf_n):
-            k = self.G(j, i)
-            if ans[k] == 0:
-                ans[k] = 1.0
-            ans[k] *= exp(self.a_m[j])
+            ans[self.clf[j].predict(self.x_test[0][i])] += 1
         maxT = 0
         maxN = ans[0]
         for j in xrange(1, len(ans)):
@@ -179,27 +131,26 @@ class AdaBoost():
 
     def finalLoss(self):
         ans = 0
-        for i in xrange(self.test_n):
-            if self.finalG(i) == self.y_test[i]:
+        for i in xrange(self.test_n[0]):
+            tmp = self.finalG(i)
+            if tmp == self.y_test[0][i]:
                 ans = ans+1
-        print 'Toatal accuracy is: ', ans*1.0/self.test_n
+        print 'The toatal accuracy is: ', ans*1.0/self.test_n[0]
     
     def partLoss(self):
         for j in xrange(self.clf_n):
             ans = 0
-            for i in xrange(self.test_n):
-                if self.y_test[i] == self.G(j, i):
+            for i in xrange(self.test_n[j]):
+                if self.y_test[j][i] == self.clf[j].predict(self.x_test[j][i]):
                     ans += 1
-            print '{0}th'.format(j+1), 'clf accuracy is: ', ans*1.0/self.test_n
+            print 'The {0}th'.format(j+1), 'clf accuracy is: ', ans*1.0/self.test_n[j]
 
-a = AdaBoost(5)
+a = Bagging(5)
 #a.showKind('zero')
 #a.showKind('one')
 #a.showKind('two')
 
 a.main()
-
-print a.a_m
 
 a.partLoss()
 
