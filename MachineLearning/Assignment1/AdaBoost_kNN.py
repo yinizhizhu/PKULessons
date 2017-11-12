e839a7289bc83306244310a9bb47fb741d716616
yinizhizhu <lijiahe19941013@sina.cn>
2017-11-12 13:54:49 +0800

    udpat

--- MachineLearning/Assignment1/AdaBoost_kNN.py
+++ MachineLearning/Assignment1/AdaBoost_kNN.py
@@ -1,12 +1,8 @@
 from math import *
+from time import clock
+from sklearn import neighbors
 
-class employee:
-    pass
-
-class knn():
-    def __init__(self, k):
-        self.k = k
-        
+start = clock()
 
 class AdaBoost():
     def __init__(self, n):
@@ -37,6 +33,7 @@ class AdaBoost():
             
                Total     28056
         '''
+        
         self.input_file = 'data\krkopt.data'
         self.attr_len = 6
         
@@ -44,16 +41,15 @@ class AdaBoost():
         
         self.loadData()
         
-        self.train_test = [0 for i in xrange(len(self.map))]
         self.AdaSample()
         
-        self.weight = [1.0/self.train_n for i in xrange(self.train_n)]
+        self.weight = [1.0/self.test_n for i in xrange(self.test_n)]
         
         
         self.clf_n = n
-        self.clf = [i+1 for i in xrange(self.clf_n)]
-        
-    
+        self.clf = [neighbors.KNeighborsClassifier(n_neighbors = 15+i)
+                                                   for i in xrange(self.clf_n)]
+
     def getMap(self):
         self.map = dict()
         self.verse_map = dict()
@@ -66,12 +62,13 @@ class AdaBoost():
                 self.verse_map[i] = one
                 i = i+1
         read.close()
-    
+        self.train_test = [0 for j in xrange(len(self.map))]
+
     def insertData(self, words):
         kind = words[self.attr_len]
-        tmp = [ord(words[i])/256.0 for i in xrange(self.attr_len-1)]
+        tmp = [ord(words[i])/256.0 for i in xrange(self.attr_len)]
         self.data[self.map[kind]].append(tmp)
-    
+
     def showKind(self, kind):
         if type(kind) == str:
             kind = self.map[kind]
@@ -79,33 +76,31 @@ class AdaBoost():
         print 'The number of {0} is {1}: '.format(kind, n)
         for i in xrange(len(self.data[kind])):
             print self.data[kind][i]
-    
+
     def AdaSample(self):
         self.x = []
         self.y = []
         self.train_n = 0
-        
+
         self.x_test = []
         self.y_test = []
         self.test_n = 0
 
         for i in xrange(len(self.map)):
             n = len(self.data[i])
-            n_train = int(n*0.5)
+            n_train = int(n*0.8)
             self.train_test[i] = n_train
             
             for j in xrange(n_train):
                 self.x.append(self.data[i][j])
-            for j in xrange(n_train):
                 self.y.append(i)
             self.train_n += n_train
             
             for j in xrange(n_train, n):
                 self.x_test.append(self.data[i][j])
-            for j in xrange(n_train, n):
                 self.y_test.append(i)
             self.test_n += n - n_train
-        
+
     def loadData(self):
         data = open(self.input_file, 'r')
         self.data = [[] for i in xrange(len(self.map))]
@@ -113,119 +108,105 @@ class AdaBoost():
             words = line.strip('\n').split(',')
             self.insertData(words)
         data.close()
-    
+
     def I(self, value):
         if value != 0:
             return 1
         return 0
-    
+
     def judge(self, value):
         return self.verse_map[value]
-    
+
     def G(self, m, i):
-        return self.fit(self.x_test[i], self.clf[m])
-    
+        return self.clf[m].predict(self.x_test[i])[0]
+
     def Em(self, m):
         em = 0.0
+        G_m = []
         for i in xrange(self.test_n):
-            if self.G(m, i) != self.y_test[i]:
+            tmp = self.G(m, i)
+            G_m.append(tmp)
+            if tmp != self.y_test[i]:
                 em = em + self.weight[i]
-        return em
-        
+        return [em, G_m]
+
     def Aerfa(self, m):
 #        print m, 
-        em = self.Em(m)
+        [em, G_m] = self.Em(m)
 #        print em
-        return 0.5*log((1-em)/em)
-        
+        return [0.5*log((1-em)/em), G_m]
+
     def step(self, m):
-        aerfa = self.Aerfa(m)
+        print self.weight[0]
+        [aerfa, G_m] = self.Aerfa(m)
         z = 0
-        for i in xrange(self.sample_n):
-            tmp = self.weight[i]*math.exp(-aerfa*self.y_test[i]*self.G(m, i))
+        for i in xrange(self.test_n):
+            if self.y_test[i] == G_m[i]:
+                tmp = self.weight[i]*exp(-aerfa)
+            else:
+                tmp = self.weight[i]*exp(aerfa)
             self.weight[i] = tmp
             z = z+tmp
-        for i in xrange(self.sample_n):
+        for i in xrange(self.test_n):
             self.weight[i] /= z
 #        print aerfa
         self.a_m.append(aerfa)
 
     def main(self):
-        for i in xrange(self.clf_n):
-            self.step(i)
-    
-#    def partG(self, m, i):
-#        return self.judge(self.G(m, i))
-#            
-#    def partLoss(self, m):
-#        ans = 0
-#        for i in xrange(self.test_n):
-#            if self.partG(m, i) != self.y[i]:
-#                ans = ans+1
-#        return ans
-    
+        self.a_m = []
+        for m in xrange(self.clf_n):
+            s = clock()
+            self.clf[m].fit(self.x, self.y)
+            self.step(m)
+            e = clock()
+            print self.a_m
+            print "The step time is: ", e-s
+
     def finalG(self, i):
         ans = [0 for j in xrange(len(self.map))]
         for j in xrange(self.clf_n):
-            ans[self.G(j, i)] += self.a_m[j]
+            ans[self.G(j, i)] += self.a_m[j]+1
         maxT = 0
         maxN = ans[0]
-        for j in xrange(1, len(self.ans)):
+        for j in xrange(1, len(ans)):
             if ans[j] > maxN:
                 maxN = ans[j]
                 maxT = j
-        return maxT
-    
+        return  maxT
+
     def finalLoss(self):
         ans = 0
         for i in xrange(self.test_n):
             tmp = self.finalG(i)
             if tmp != self.y_test[i]:
-                print self.x_test[i], tmp, self.y_test[i]
                 ans = ans+1
-        return ans
+        print 'The toatal accuracy is: ', ans*1.0/self.test_n
     
-    '''
-        For the kNN
-    '''
-    def dis(self, node, i):
-        ans = 0
-        for j in xrange(len(node)):
-            delta = self.x[i][j]-node[j]
-            ans = ans + delta*delta
-        return sqrt(ans)
-    
-    def getMost(self, ans):
-        res = [0 for i in xrange(len(self.map))]
-        for i in xrange(len(ans)):
-            res[ans[i]] = res[ans[i]] + 1
-        maxT = 0
-        maxN = res[0]
-        for i in xrange(1, len(res)):
-            if res[i] > maxN:
-                maxT = i
-                maxN = res[i]
-        return maxT
-    
-    def fit(self, node, k):
-        distance = []
-#        print node
-        for i in xrange(len(self.y)):
-            tmp = employee()
-            tmp.type = self.y[i]
-            tmp.dis = self.dis(node, i)
-            distance.append(tmp)
-        
-        distance.sort(key = lambda d:d.dis)
-        ans = []
-        for i in xrange(k):
-            ans.append(distance[i].type)
-            
-        return self.getMost(ans)
+    def partLoss(self):
+        for j in xrange(self.clf_n):
+            ans = 0
+            for i in xrange(self.test_n):
+                if self.y_test[i] == self.G(j, i):
+                    ans += 1
+            print 'The {0}th'.format(j+1), 'clf accuracy is: ', ans*1.0/self.test_n
 
-a = AdaBoost(3)
-a.showKind('zero')
-a.showKind('one')
-a.showKind('two')
+a = AdaBoost(5)
+#a.showKind('zero')
+#a.showKind('one')
+#a.showKind('two')
+
 a.main()
-print a.finalLoss()
+
+print a.a_m
+
+a.partLoss()
+
+a.finalLoss()
+
+
+end = clock()
+print "The total time is: ", end-start
+
+#
+#tmp = neighbors.KNeighborsClassifier(algorithm='auto')
+#print tmp.__doc__
