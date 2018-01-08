44c5405321dbc7ccb49d621cf90579a55b8e285b
yinizhizhu <lijiahe19941013@sina.cn>
2018-01-08 10:36:40 +0800

    Updaet

---
+++ Algorithms/Final/4F.cpp
@@ -0,0 +1,120 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+#include <string.h>
+#include <assert.h>
+#include <queue>
+#include <vector>
+#include <algorithm>
+
+#define N (430)
+#define M (N*N+4*N)
+typedef long long LL;
+
+using namespace std;
+
+struct edge {
+	int v, cap, next;
+};
+edge e[M];
+
+int head[N], level[N], cur[N];
+int num_of_edges;
+
+void dinic_init(void) {
+	num_of_edges = 0;
+	memset(head, -1, sizeof(head));
+	return;
+}
+
+int add_edge(int u, int v, int c1, int c2) {
+	int& i = num_of_edges;
+
+	assert(c1 >= 0 && c2 >= 0 && c1 + c2 >= 0); // check for possibility of overflow
+	e[i].v = v;
+	e[i].cap = c1;
+	e[i].next = head[u];
+	head[u] = i++;
+
+	e[i].v = u;
+	e[i].cap = c2;
+	e[i].next = head[v];
+	head[v] = i++;
+	return i;
+}
+
+int dfs(int u, int t, int bn) {
+	if (u == t) return bn;
+	int left = bn;
+	for (int &i = cur[u]; i >= 0; i = e[i].next) {
+		int v = e[i].v;
+		int c = e[i].cap;
+		if (c > 0 && level[u] + 1 == level[v]) {
+			int flow = dfs(v, t, min(left, c));
+			if (flow > 0) {
+				e[i].cap -= flow;
+				e[i ^ 1].cap += flow;
+				cur[u] = i;
+				left -= flow;
+				if (!left) break;
+			}
+		}
+	}
+	if (left > 0) level[u] = 0;
+	return bn - left;
+}
+
+bool bfs(int s, int t) {
+	memset(level, 0, sizeof(level));
+	level[s] = 1;
+	queue<int> q;
+	q.push(s);
+	while (!q.empty()) {
+		int u = q.front();
+		q.pop();
+		if (u == t) return true;
+		for (int i = head[u]; i >= 0; i = e[i].next) {
+			int v = e[i].v;
+			if (!level[v] && e[i].cap > 0) {
+				level[v] = level[u] + 1;
+				q.push(v);
+			}
+		}
+	}
+	return false;
+}
+
+LL dinic(int s, int t) {
+	LL max_flow = 0;
+
+	while (bfs(s, t)) {
+		memcpy(cur, head, sizeof(head));
+		max_flow += dfs(s, t, INT_MAX);
+	}
+	return max_flow;
+}
+
+int main() {
+	dinic_init();
+	int n, f, d, i, k, lf, ld;
+	scanf("%d %d %d", &n, &f, &d);
+	int d_b = f + (n << 1), t = d_b + d + 1;
+	for (i = 1; i <= f; i++)
+		add_edge(0, i, 1, 0);
+	for (i = 1; i <= d; i++)
+		add_edge(d_b + i, t, 1, 0);
+	for (i = 1; i <= n; i++) {
+		scanf("%d %d", &lf, &ld);
+		for (; lf; lf--) {
+			scanf("%d", &k);
+			add_edge(k, f + i, 1, 0);
+		}
+		for (; ld; ld--) {
+			scanf("%d", &k);
+			add_edge(f + n + i, d_b + k, 1, 0);
+		}
+		add_edge(f + i, f + n + i, 1, 0);
+	}
+	printf("%lld\n", dinic(0, t));
+	return 0;
+}
